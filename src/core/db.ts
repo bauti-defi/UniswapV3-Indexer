00f952bb04955f4056567128d220698bdeffa2f2
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2023-09-20 19:11:48 -0300

    fix: multichain same db

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,4 +1,9 @@
 import {Store as Store_, TypeormDatabase} from '@subsquid/typeorm-store'
+import { chainId } from '../utils/chain'
 
-export let db = new TypeormDatabase()
+const name = `squid_process_${chainId()}`
+
+export let db = new TypeormDatabase({
+    stateSchema: name
+})
 export type Store = Store_
